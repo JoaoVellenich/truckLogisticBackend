956d9cc65c0142439c56f94d35d7a34cd02d906f
JoaoVellenich <jpvellenich@gmail.com>
2024-05-21 21:40:09 -0300

    feat: start project

---
+++ src/main/java/com/joaovellenich/TruckLogistic/TruckLogisticApplication.java
@@ -0,0 +1,13 @@
+package com.joaovellenich.TruckLogistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckLogisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckLogisticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaovellenich/TruckLogistic/TruckLogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovellenich.TruckLogistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckLogisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
