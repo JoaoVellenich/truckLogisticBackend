c321f979ac0e21079163818f8e6bd0b73232240d
JoaoVellenich <jpvellenich@gmail.com>
2024-05-23 10:20:16 -0300

    feat: creating user repository

---
+++ src/main/java/com/joaovellenich/TruckLogistic/infra/gateways/UserRepositoryGateway.java
@@ -0,0 +1,27 @@
+package com.joaovellenich.TruckLogistic.infra.gateways;
+
+import com.joaovellenich.TruckLogistic.application.gateways.UserGateway;
+import com.joaovellenich.TruckLogistic.infra.persistence.entities.UserEntity;
+import com.joaovellenich.TruckLogistic.infra.persistence.mapper.UserMapper;
+import com.joaovellenich.TruckLogistic.infra.persistence.repositories.UserRepository;
+import com.joaovellenich.TruckLogistic.model.User;
+
+public class UserRepositoryGateway implements UserGateway {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    public UserRepositoryGateway(UserRepository userRepository, UserMapper userMapper){
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+    @Override
+    public boolean hasUserWithEmail(String email) {
+        return this.userRepository.existsByEmail(email);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        UserEntity userToSave = this.userMapper.toEntity(user);
+        UserEntity userSaved = this.userRepository.save(userToSave);
+        return this.userMapper.toDomain(userSaved);
+    }
+}
