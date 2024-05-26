aaad7695b576b08545f1c54c74eb81d41ac81b77
JoaoVellenich <jpvellenich@gmail.com>
2024-05-26 19:52:50 -0300

    feat: adding create routes injection

--- src/main/java/com/joaovellenich/TruckLogistic/config/UserConfig.java
+++ src/main/java/com/joaovellenich/TruckLogistic/config/UserConfig.java
@@ -2,12 +2,15 @@ package com.joaovellenich.TruckLogistic.config;
 
 import com.joaovellenich.TruckLogistic.application.gateways.UserGateway;
 import com.joaovellenich.TruckLogistic.application.useCase.user.CreateUserUseCase;
+import com.joaovellenich.TruckLogistic.application.useCase.user.LoginUserUseCase;
 import com.joaovellenich.TruckLogistic.dto.user.create.CreateUserDTOMapper;
 import com.joaovellenich.TruckLogistic.infra.gateways.UserRepositoryGateway;
 import com.joaovellenich.TruckLogistic.infra.persistence.mapper.UserMapper;
 import com.joaovellenich.TruckLogistic.infra.persistence.repositories.UserRepository;
+import com.joaovellenich.TruckLogistic.infra.security.TokenService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,6 +19,11 @@ public class UserConfig {
     @Bean
     public CreateUserUseCase createUserUseCase(UserGateway userGateway, PasswordEncoder passwordEncoder){
         return new CreateUserUseCase(userGateway, passwordEncoder);
+    }
+
+    @Bean
+    public LoginUserUseCase loginUserUseCase(TokenService tokenService, AuthenticationManager authenticationManager){
+        return new LoginUserUseCase(tokenService, authenticationManager);
     }
 
     @Bean
@@ -33,8 +41,4 @@ public class UserConfig {
         return new CreateUserDTOMapper();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
