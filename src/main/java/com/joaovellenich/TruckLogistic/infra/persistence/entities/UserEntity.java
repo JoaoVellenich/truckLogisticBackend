40ea97e20fa8d94c7f06dc36fc42a21f3b3ab21b
JoaoVellenich <jpvellenich@gmail.com>
2024-05-26 19:53:13 -0300

    feat: fix create and update timestamp

--- src/main/java/com/joaovellenich/TruckLogistic/infra/persistence/entities/UserEntity.java
+++ src/main/java/com/joaovellenich/TruckLogistic/infra/persistence/entities/UserEntity.java
@@ -1,20 +1,31 @@
 package com.joaovellenich.TruckLogistic.infra.persistence.entities;
 
+import com.joaovellenich.TruckLogistic.model.UserRole;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
 @Builder
 @Getter
-public class UserEntity {
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -24,11 +35,45 @@ public class UserEntity {
     private String email;
     @Column(nullable = false)
     private String password;
-
-    @CreatedDate
+    private UserRole role;
+    @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createAt;
-    @LastModifiedDate
+    @UpdateTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN){
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        }else{
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
