e89c06a599c2368226d461ca75793b73e29c4457
JoaoVellenich <jpvellenich@gmail.com>
2024-05-26 19:52:35 -0300

    fix: create and update timestamp

--- src/main/java/com/joaovellenich/TruckLogistic/model/User.java
+++ src/main/java/com/joaovellenich/TruckLogistic/model/User.java
@@ -18,6 +18,7 @@ public class User {
     private String name;
     private String email;
     private String password;
+    private UserRole role;
 
     private LocalDateTime createAt;
     private LocalDateTime updatedAt;
