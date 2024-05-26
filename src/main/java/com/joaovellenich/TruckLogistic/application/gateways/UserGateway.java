3a7a6f760fc6f09a407b9af2641ed04869db60ac
JoaoVellenich <jpvellenich@gmail.com>
2024-05-26 19:54:34 -0300

    feat: adding function to user gateway

--- src/main/java/com/joaovellenich/TruckLogistic/application/gateways/UserGateway.java
+++ src/main/java/com/joaovellenich/TruckLogistic/application/gateways/UserGateway.java
@@ -1,8 +1,12 @@
 package com.joaovellenich.TruckLogistic.application.gateways;
 
 import com.joaovellenich.TruckLogistic.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface UserGateway {
     boolean hasUserWithEmail(String email);
+    UserDetails findByEmail(String email);
     User saveUser(User user);
 }
