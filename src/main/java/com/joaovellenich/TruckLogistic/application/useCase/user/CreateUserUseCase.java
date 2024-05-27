ccc6f87ad0faa45803bb8d7b48a5f7df2c202b44
JoaoVellenich <jpvellenich@gmail.com>
2024-05-27 17:58:27 -0300

    fix: CreateUserUseCase to receive company

--- src/main/java/com/joaovellenich/TruckLogistic/application/useCase/user/CreateUserUseCase.java
+++ src/main/java/com/joaovellenich/TruckLogistic/application/useCase/user/CreateUserUseCase.java
@@ -1,23 +1,41 @@
 package com.joaovellenich.TruckLogistic.application.useCase.user;
 
+import com.joaovellenich.TruckLogistic.application.gateways.CompanyGateway;
 import com.joaovellenich.TruckLogistic.application.gateways.UserGateway;
+import com.joaovellenich.TruckLogistic.dto.user.create.CreateUserRequestDTO;
+import com.joaovellenich.TruckLogistic.model.Company;
 import com.joaovellenich.TruckLogistic.model.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
+
 public class CreateUserUseCase {
     private final UserGateway userGateway;
+    private final CompanyGateway companyGateway;
     private final PasswordEncoder encoder;
 
-    public CreateUserUseCase(UserGateway userGateway, PasswordEncoder encoder){
+    public CreateUserUseCase(UserGateway userGateway, CompanyGateway companyGateway, PasswordEncoder encoder){
         this.userGateway = userGateway;
+        this.companyGateway = companyGateway;
         this.encoder = encoder;
     }
 
-    public User execute(User user) throws Exception{
-        boolean hasUser = this.userGateway.hasUserWithEmail(user.getEmail());
+    public User execute(CreateUserRequestDTO user) throws Exception{
+        Company userCompany = this.companyGateway.findById(user.companyId());
+        if(userCompany == null){
+            throw new Exception("Company not found");
+        }
+        boolean hasUser = this.userGateway.hasUserWithEmail(user.email());
         if(!hasUser){
-            user.setPassword(encoder.encode(user.getPassword()));
-            return  this.userGateway.saveUser(user);
+            User newUser = User.builder()
+                    .name(user.name())
+                    .email(user.email())
+                    .password(this.encoder.encode(user.password()))
+                    .company(userCompany)
+                    .role(user.role())
+                    .build();
+
+            return this.userGateway.saveUser(newUser);
         }else{
             throw new Exception("User already exists on database");
         }
