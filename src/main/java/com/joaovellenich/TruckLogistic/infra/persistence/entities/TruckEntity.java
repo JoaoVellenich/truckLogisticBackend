56a6db6723d436497f60b14fb18008bd3aed2086
JoaoVellenich <jpvellenich@gmail.com>
2024-05-29 10:25:04 -0300

    feat: creating truck entity

---
+++ src/main/java/com/joaovellenich/TruckLogistic/infra/persistence/entities/TruckEntity.java
@@ -0,0 +1,29 @@
+package com.joaovellenich.TruckLogistic.infra.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "trucks")
+public class TruckEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @ManyToOne
+    @JoinColumn(name = "company_id", nullable = false)
+    private CompanyEntity company;
+    @Column(nullable = false, unique = true)
+    private String plate;
+    private String description;
+    @Column(nullable = false)
+    private double km;
+    @Column(nullable = false)
+    private int numberOfAxle;
+}
