58e87a9cfff130dd095cbe5b6957cb1bef6ce8e7
JoaoVellenich <jpvellenich@gmail.com>
2024-05-29 10:25:32 -0300

    feat: creating truck mapper

---
+++ src/main/java/com/joaovellenich/TruckLogistic/infra/persistence/mapper/TruckMapper.java
@@ -0,0 +1,34 @@
+package com.joaovellenich.TruckLogistic.infra.persistence.mapper;
+
+import com.joaovellenich.TruckLogistic.infra.persistence.entities.TireEntity;
+import com.joaovellenich.TruckLogistic.infra.persistence.entities.TruckEntity;
+import com.joaovellenich.TruckLogistic.model.Truck;
+
+import java.util.stream.Collectors;
+
+public class TruckMapper {
+    private final CompanyMapper companyMapper;
+    public TruckMapper(CompanyMapper companyMapper){
+        this.companyMapper = companyMapper;
+    }
+    public TruckEntity toEntity(Truck truck){
+        return TruckEntity.builder()
+                .id(truck.getId())
+                .company(this.companyMapper.toEntity(truck.getCompany()))
+                .plate(truck.getPlate())
+                .description(truck.getDescription())
+                .km(truck.getKm())
+                .numberOfAxle(truck.getNumberOfAxle())
+                .build();
+    }
+    public Truck toDomain(TruckEntity truck){
+        return Truck.builder()
+                .id(truck.getId())
+                .company(this.companyMapper.toDomain(truck.getCompany()))
+                .plate(truck.getPlate())
+                .description(truck.getDescription())
+                .km(truck.getKm())
+                .numberOfAxle(truck.getNumberOfAxle())
+                .build();
+    }
+}
