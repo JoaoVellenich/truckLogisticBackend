1f38ddea03e93cffb5b3aac341bf3ac015294a16
JoaoVellenich <jpvellenich@gmail.com>
2024-07-25 15:17:03 -0300

    feat: adding new use case to config file

--- src/main/java/com/joaovellenich/TruckLogistic/config/TruckConfig.java
+++ src/main/java/com/joaovellenich/TruckLogistic/config/TruckConfig.java
@@ -4,6 +4,7 @@ import com.joaovellenich.TruckLogistic.application.gateways.TireGateway;
 import com.joaovellenich.TruckLogistic.application.gateways.TruckGateway;
 import com.joaovellenich.TruckLogistic.application.gateways.UserGateway;
 import com.joaovellenich.TruckLogistic.application.useCase.truck.CreateTruckUseCase;
+import com.joaovellenich.TruckLogistic.application.useCase.truck.GetTruckByIdUseCase;
 import com.joaovellenich.TruckLogistic.application.useCase.truck.GetTrucksInfoUseCase;
 import com.joaovellenich.TruckLogistic.dto.truck.getTruckInfo.GetTrucksInfoDTOMapper;
 import com.joaovellenich.TruckLogistic.infra.controller.TruckController;
@@ -18,16 +19,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TruckConfig {
     @Bean
-    public TruckController truckController(CreateTruckUseCase createTruckUseCase, GetTrucksInfoUseCase getTrucksInfoUseCase, GetTrucksInfoDTOMapper getTrucksInfoDTOMapper){
-        return new TruckController(createTruckUseCase, getTrucksInfoUseCase, getTrucksInfoDTOMapper);
+    public TruckController truckController(CreateTruckUseCase createTruckUseCase, GetTrucksInfoUseCase getTrucksInfoUseCase, GetTrucksInfoDTOMapper getTrucksInfoDTOMapper, GetTruckByIdUseCase getTruckByIdUseCase){
+        return new TruckController(createTruckUseCase, getTrucksInfoUseCase, getTrucksInfoDTOMapper, getTruckByIdUseCase);
     }
     @Bean
     public CreateTruckUseCase createTruckUseCase(TruckGateway truckGateway, UserGateway userGateway, TireGateway tireGateway){
         return new CreateTruckUseCase(truckGateway, userGateway, tireGateway);
     }
     @Bean
-    public GetTrucksInfoUseCase getTruckUseCase(TruckGateway truckGateway){
-        return new GetTrucksInfoUseCase(truckGateway);
+    public GetTrucksInfoUseCase getTruckUseCase(TruckGateway truckGateway, UserGateway userGateway){
+        return new GetTrucksInfoUseCase(truckGateway, userGateway);
+    }
+    @Bean
+    public GetTruckByIdUseCase getTruckByIdUseCase(TruckGateway truckGateway, TireGateway tireGateway){
+        return new GetTruckByIdUseCase(truckGateway, tireGateway);
     }
     @Bean
     public TruckGateway truckGateway(TruckRepository truckRepository, TruckMapper truckMapper){
