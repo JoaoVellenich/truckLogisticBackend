1fee67984538fb7b69d5cc0c3af2c5d3d77bc869
JoaoVellenich <jpvellenich@gmail.com>
2024-07-26 10:57:22 -0300

    feat: adding getFuelPage to config

--- src/main/java/com/joaovellenich/TruckLogistic/config/FuelConfig.java
+++ src/main/java/com/joaovellenich/TruckLogistic/config/FuelConfig.java
@@ -5,6 +5,7 @@ import com.joaovellenich.TruckLogistic.application.gateways.TireGateway;
 import com.joaovellenich.TruckLogistic.application.gateways.TruckGateway;
 import com.joaovellenich.TruckLogistic.application.gateways.UserGateway;
 import com.joaovellenich.TruckLogistic.application.useCase.fuel.CreateFuelUseCase;
+import com.joaovellenich.TruckLogistic.application.useCase.fuel.GetFuelPageUseCase;
 import com.joaovellenich.TruckLogistic.infra.controller.FuelController;
 import com.joaovellenich.TruckLogistic.infra.gateways.FuelRepositoryGateway;
 import com.joaovellenich.TruckLogistic.infra.persistence.mapper.FuelMapper;
@@ -16,12 +17,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FuelConfig {
     @Bean
-    public FuelController fuelController(CreateFuelUseCase createFuelUseCase){
-        return new FuelController(createFuelUseCase);
+    public FuelController fuelController(CreateFuelUseCase createFuelUseCase, GetFuelPageUseCase getFuelPageUseCase){
+        return new FuelController(createFuelUseCase, getFuelPageUseCase);
     }
     @Bean
     public CreateFuelUseCase createFuelUseCase(UserGateway userGateway, TruckGateway truckGateway, FuelGateway fuelGateway, TireGateway tireGateway){
         return new CreateFuelUseCase(userGateway, truckGateway, fuelGateway, tireGateway);
+    }
+    @Bean
+    public GetFuelPageUseCase getFuelPageUseCase(UserGateway userGateway, TruckGateway truckGateway, FuelGateway fuelGateway){
+        return new GetFuelPageUseCase(userGateway, fuelGateway, truckGateway);
     }
     @Bean
     public FuelGateway fuelGateway(FuelRepository fuelRepository, FuelMapper fuelMapper){
