adf3fbc6773a2cbabb4eec2159cdaead49b5cfdb
JoaoVellenich <jpvellenich@gmail.com>
2024-07-26 09:47:48 -0300

    feat: create fuel mapper

---
+++ src/main/java/com/joaovellenich/TruckLogistic/infra/persistence/mapper/FuelMapper.java
@@ -0,0 +1,42 @@
+package com.joaovellenich.TruckLogistic.infra.persistence.mapper;
+
+import com.joaovellenich.TruckLogistic.infra.persistence.entities.FuelEntity;
+import com.joaovellenich.TruckLogistic.model.Fuel;
+
+public class FuelMapper {
+    private final TruckMapper truckMapper;
+
+    public FuelMapper(TruckMapper truckMapper) {
+        this.truckMapper = truckMapper;
+    }
+
+    public Fuel toDomain(FuelEntity fuel){
+        return Fuel.builder()
+                .id(fuel.getId())
+                .date(fuel.getDate())
+                .truck(this.truckMapper.toDomain(fuel.getTruck()))
+                .value(fuel.getValue())
+                .km(fuel.getKm())
+                .trip(fuel.getTrip())
+                .kmL(fuel.getKmL())
+                .price(fuel.getPrice())
+                .liters(fuel.getLiters())
+                .location(fuel.getLocation())
+                .build();
+    }
+
+    public FuelEntity toEntity(Fuel fuel){
+        return FuelEntity.builder()
+                .id(fuel.getId())
+                .date(fuel.getDate())
+                .truck(this.truckMapper.toEntity(fuel.getTruck()))
+                .value(fuel.getValue())
+                .km(fuel.getKm())
+                .trip(fuel.getTrip())
+                .kmL(fuel.getKmL())
+                .price(fuel.getPrice())
+                .liters(fuel.getLiters())
+                .location(fuel.getLocation())
+                .build();
+    }
+}
