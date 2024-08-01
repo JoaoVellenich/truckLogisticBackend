f0d252a9d044b948d30d5a787d972bf28109b670
JoaoVellenich <jpvellenich@gmail.com>
2024-08-01 10:12:28 -0300

    feat: tire config injection

--- src/main/java/com/joaovellenich/TruckLogistic/config/TireConfig.java
+++ src/main/java/com/joaovellenich/TruckLogistic/config/TireConfig.java
@@ -1,6 +1,10 @@
 package com.joaovellenich.TruckLogistic.config;
 
 import com.joaovellenich.TruckLogistic.application.gateways.TireGateway;
+import com.joaovellenich.TruckLogistic.application.gateways.TruckGateway;
+import com.joaovellenich.TruckLogistic.application.gateways.UserGateway;
+import com.joaovellenich.TruckLogistic.application.useCase.tire.ChangeTireUseCase;
+import com.joaovellenich.TruckLogistic.infra.controller.TireController;
 import com.joaovellenich.TruckLogistic.infra.gateways.TireRepositoryGateway;
 import com.joaovellenich.TruckLogistic.infra.persistence.mapper.TireMapper;
 import com.joaovellenich.TruckLogistic.infra.persistence.repositories.TireRepository;
@@ -9,6 +13,14 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TireConfig {
+    @Bean
+    public TireController tireController(ChangeTireUseCase changeTireUseCase){
+        return new TireController(changeTireUseCase);
+    }
+    @Bean
+    public ChangeTireUseCase changeTireUseCase(UserGateway userGateway, TireGateway tireGateway, TruckGateway truckGateway){
+        return new ChangeTireUseCase(userGateway, tireGateway, truckGateway);
+    }
     @Bean
     public TireGateway tireGateway(TireRepository tireRepository, TireMapper tireMapper){
         return new TireRepositoryGateway(tireRepository, tireMapper);
